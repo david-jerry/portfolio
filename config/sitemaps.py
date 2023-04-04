841f205adeed3be261c720aa6533eb666884247a
Jeremiah David <jeremiahedavid@gmail.com>
2023-04-03 18:16:03 -0600

    added buildpack

--- config/sitemaps.py
+++ config/sitemaps.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 
 class StaticViewSitemap(Sitemap):
     def items(self):
-        return ["home", "about", "privacy", "terms", "contact", "cookies", "copyright"]
+        return ["home", "about"] # "privacy", "terms", "contact", "cookies", "copyright"
 
     def location(self, item):
         return reverse(item)
